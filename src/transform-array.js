e1fbe4fae095f294472f4a36b3a425daee23fca4
lomserg <lomserg@gmail.com>
2023-06-30 15:22:03 +0300

    add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const newArr = [...arr];
+
+  let result = newArr.reduce((acc, cur, i) => {
+    if (cur === '--discard-next') {
+      if (i + 1 < newArr.length) {
+        newArr[i + 1] = undefined; // Mark the next element for removal
+      }
+    } else if (cur === '--discard-prev') {
+      if (i > 0 && newArr[i - 1] !== undefined) {
+        acc.pop();
+      }
+    } else if (cur === '--double-next') {
+      if (i + 1 < newArr.length) {
+        acc.push(newArr[i + 1]);
+      }
+    } else if (cur === '--double-prev') {
+      if (i > 0 && newArr[i - 1] !== undefined) {
+        acc.push(newArr[i - 1]);
+      }
+    } else {
+      acc.push(cur);
+    }
+
+    return acc;
+  }, []);
+
+  return result.filter((item) => item !== undefined);
 }
 
+const result = transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
+console.log(result)
+// const arr = [1, 2, 3, '--discard-next', 1337, 4, 5]
+// console.log(arr.splice(4))
 module.exports = {
   transform
 };
